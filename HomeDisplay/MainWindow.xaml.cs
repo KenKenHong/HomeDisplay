35d50b69f8a4f01353e5c7a6c33e93ca41a956d1
KenKenHong <kenkenh94@gmail.com>
2016-01-11 14:49:43 -0600

    Weather change functional

--- HomeDisplay/MainWindow.xaml.cs
+++ HomeDisplay/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Net;
+using System.IO;
 
 namespace HomeDisplay
 {
@@ -23,16 +25,19 @@ namespace HomeDisplay
     /// </summary>
     public partial class MainWindow : Window
     {
-        DispatcherTimer dispatcherTimer1 = new DispatcherTimer();
-        DispatcherTimer dispatcherTimer2 = new DispatcherTimer();
-        string currTemp;
-        string currCond;
-        string currHumid;
-        string currWind;
-        string location;
-        string foreCond;
-        string foreHigh;
-        string foreLow;
+        private DispatcherTimer dispatcherTimer1 = new DispatcherTimer();
+        private DispatcherTimer dispatcherTimer2 = new DispatcherTimer();
+        private string currTemp;
+        private string currCond;
+        private string currHumid;
+        private string currWind;
+        private string location;
+        private string foreCond;
+        private string foreHigh;
+        private string foreLow;
+        private string imgCode;
+        private string pubDate;
+        private BitmapImage sourceImg = new BitmapImage();
         
         public MainWindow()
         {
@@ -45,7 +50,7 @@ namespace HomeDisplay
             label1.Content = DateTime.Now.ToString("HH:mm:ss");
 
             dispatcherTimer2.Tick += dispatcherTimer2_Tick;
-            dispatcherTimer2.Interval = new TimeSpan(1, 0, 0);
+            dispatcherTimer2.Interval = new TimeSpan(0, 0, 1);
             dispatcherTimer2.Start();
 
             getWeather();
@@ -61,6 +66,16 @@ namespace HomeDisplay
         {
             getWeather();
 
+        }
+
+        public void mainPageDisable()
+        {
+            label3.Visibility = Visibility.Hidden;
+            label4.Visibility = Visibility.Hidden;
+            label6.Visibility = Visibility.Hidden;
+            label8.Visibility = Visibility.Hidden;
+            label10.Visibility = Visibility.Hidden;
+            label11.Visibility = Visibility.Hidden;
         }
 
         public void getWeather()
@@ -82,7 +97,7 @@ namespace HomeDisplay
 
             currHumid = chan.SelectSingleNode("yweather:atmosphere", dataMan).Attributes["humidity"].Value;
 
-            currWind = chan.SelectSingleNode("yweather:wind", dataMan).Attributes["speed"].Value;
+            currWind = chan.SelectSingleNode("yweather:wind", dataMan).Attributes["chill"].Value;
 
             location = chan.SelectSingleNode("yweather:location", dataMan).Attributes["city"].Value;
 
@@ -92,11 +107,63 @@ namespace HomeDisplay
 
             foreLow = chan.SelectSingleNode("item").SelectSingleNode("yweather:forecast", dataMan).Attributes["low"].Value;
 
+            imgCode = chan.SelectSingleNode("item").SelectSingleNode("yweather:condition", dataMan).Attributes["code"].Value;
+
+            pubDate = chan.SelectSingleNode("item").SelectSingleNode("yweather:condition", dataMan).Attributes["date"].Value;
+
             label3.Content = currTemp + " C";
             label4.Content = currCond;
             label6.Content = currHumid;
             label8.Content = foreHigh + " C";
             label10.Content = foreLow + " C";
+            label11.Content = "Last Updated: " + pubDate;
+
+            retrieveWeatherIconFromWeb();
+            //retrieveWeatherIconFromFolder();
+            image.Source = sourceImg;
+            if(sourceImg != null)
+            {
+                sourceImg = new BitmapImage();
+            }
+
+        }
+
+        private void retrieveWeatherIconFromFolder()
+        {
+            if (imgCode.Equals("3200"))
+            {
+                sourceImg = new BitmapImage(new Uri(sourceImg.BaseUri, "Images/na.png"));
+            }
+            else
+            {
+                sourceImg = new BitmapImage(new Uri(sourceImg.BaseUri, "Images/" + imgCode + ".png"));
+            }
+        }
+
+        private void retrieveWeatherIconFromWeb()
+        {
+            int bytesToRead = 100;
+
+            WebRequest req = WebRequest.Create(new Uri("http://l.yimg.com/a/i/us/we/52/" + imgCode + ".gif", UriKind.Absolute));
+            req.Timeout = -1;
+            WebResponse resp = req.GetResponse();
+            Stream respStr = resp.GetResponseStream();
+            BinaryReader reader = new BinaryReader(respStr);
+            MemoryStream memStr = new MemoryStream();
+
+            byte[] bytebuf = new byte[bytesToRead];
+            int bytesRead = reader.Read(bytebuf, 0, bytesToRead);
+
+            while (bytesRead > 0)
+            {
+                memStr.Write(bytebuf, 0, bytesRead);
+                bytesRead = reader.Read(bytebuf, 0, bytesToRead);
+            }
+
+            sourceImg.BeginInit();
+            memStr.Seek(0, SeekOrigin.Begin);
+            sourceImg.StreamSource = memStr;
+            sourceImg.EndInit();
         }
     }
 }
